95a84e046ed394df41d19c812f42dd3c02fa47d3
Amr <amruser97@gmail.com>
2023-09-19 07:47:20 +0300

    dev: adding basic structure

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppHeader />
+  <!-- Introduction -->
+  <AppIntro />
+  <!-- Main Content -->
+  <AppMain />
+  <!-- Player -->
+  <AppPlayer />
+  <!-- Auth Modal -->
+  <Auth />
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppIntro from './components/AppIntro.vue'
+import AppMain from './components/AppMain.vue'
+import Auth from './components/Auth.vue'
+import AppPlayer from './components/AppPlayer.vue'
+</script>
+
+<style lang="scss" scoped></style>
